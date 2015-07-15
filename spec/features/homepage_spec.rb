d58dc02a169c3c03145aecbe8077aebdb68010c3
Jared Norman <jared@freerunningtechnologies.com>
2015-07-14 19:22:26 -0700

    Move login to homepage

--- spec/features/homepage_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'feature_helper'
-
-RSpec.describe "the homepage", type: :feature do
-  scenario "visiting the homepage" do
-    visit "/"
-    expect(page).to have_content "Get Swole"
-    expect(page).to have_content "TODO"
-  end
-end
